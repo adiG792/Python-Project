f21fab48b76df629778a88657193044d480e430f
adiG792 <74980670+adiG792@users.noreply.github.com>
2021-01-01 21:19:56 +0530

    Updated

--- Vector_Product.py
+++ Vector_Product.py
@@ -9,12 +9,16 @@ def vect_product():
     b = str(i2) + "i + " + str(j2) + "j + " + str(k2) + "k"
     print("A =", a)
     print("B =", b, "\n")
-
+    add = "+"
     if i1 == i2 and j1 == j2 and k1 == k2:
         print("Zero Vector")
     else:
-        c = str((j1 * k2) - (j2 * k1)) + "i + " + str((i2 * k1) - (i1 * k2)) + "j + " + str((i1 * j2) - (i2 * j1)) + "k "
-        print(f"A x B = C = {c}")
+        c1, c2, c3 = (j1 * k2) - (j2 * k1), (i2 * k1) - (i1 * k2), (i1 * j2) - (i2 * j1)
+        if c1 != abs(c1) or c2 != abs(c2) or c3 != abs(c3):
+            add = "-"
+        c =  str(c1) + "i" + add + str(abs(c2)) + "j" + add + str(abs(c3)) + "k"
+        print("A x B = \n")
+        print(f"C = {c}")
 
 
 vect_product()
