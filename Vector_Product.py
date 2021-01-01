cf9af4dafe1863c28985ef48fa4d5508ee1ca16d
adiG792 <74980670+adiG792@users.noreply.github.com>
2021-01-01 21:50:52 +0530

    Updated

--- Vector_Product.py
+++ Vector_Product.py
@@ -1,5 +1,6 @@
 
-def vect_product():
+def vector_product():
+    import math
     print("Vector Product\n")
     print("A = a1(i) + a2(j) + a3(k)\n")
     print("B = b1(i) + b2(j) + b3(k)\n")
@@ -16,9 +17,16 @@ def vect_product():
         c1, c2, c3 = (j1 * k2) - (j2 * k1), (i2 * k1) - (i1 * k2), (i1 * j2) - (i2 * j1)
         if c1 != abs(c1) or c2 != abs(c2) or c3 != abs(c3):
             add = "-"
-        c =  str(c1) + "i" + add + str(abs(c2)) + "j" + add + str(abs(c3)) + "k"
+        c = str(c1) + "i" + add + str(abs(c2)) + "j" + add + str(abs(c3)) + "k"
         print("A x B = \n")
-        print(f"C = {c}")
+        print(f"C = {c}\n")
+        _c = math.sqrt(pow(c1, 2) + pow(c2, 2) + pow(c3, 2))
+        a = round(_c, 2)
+        print(f"|C| = {a}\n")
+        _c1 = "(" + str(c1) + "/" + str(a) + ")" + "i"
+        _c2 = "(" + str(abs(c2)) + "/" + str(a) + ")" + "j"
+        _c3 = "(" + str(abs(c3)) + "/" + str(a) + ")" + "k"
+        __c = _c1 + add + _c2 + add + _c3
+        print(f"C/|C| = {__c}")
 
-
-vect_product()
+vector_product()
