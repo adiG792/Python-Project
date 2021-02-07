5698ee6d14956f34261eed95378fd5cbc472808a
Aditya <starlod792@gmail.com>
2021-02-07 17:05:24 +0530

    Modules_Pip

---
+++ Modules_pip/Modules_Pip.py
@@ -0,0 +1,15 @@
+import imp_file
+
+print(imp_file.roll_dice(10))
+# we have thousands of external modules in lib/ in python source code
+# we can also install third party files
+
+
+# installing third party modules
+# by installing pip(aka package manager)
+# install by command prompt
+# check if python is installed or not, ( pip --version )
+# pip install (python module you want to install without bracket)
+# module is installed in /lib/site-packages
+# to uninstall
+# pip uninstall (python module you want to uninstall without bracket)
