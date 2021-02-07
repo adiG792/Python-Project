138a319188ec116c43b6747a135cbfe55848eabf
Aditya <starlod792@gmail.com>
2021-02-07 17:07:46 +0530

    example

---
+++ Modules_pip/imp_file.py
@@ -0,0 +1,15 @@
+
+# example file for Modules_pip
+import random
+
+feet_in_mile = 5280
+meters_in_kilometer = 1000
+beatles = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star"]
+
+
+def get_file_ext(filename):
+    return filename[filename.index(".") + 1:]
+
+
+def roll_dice(num):
+    return random.randint(1, num)
